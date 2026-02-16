541d7b9599b5556b52e795a1e367c767d1ecb88d
cucuv007 <42567303+cucuv007@users.noreply.github.com>
2026-02-16 09:47:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
 
-//const SUPABASE_URL = 'https://yfiulwcqyxgvpiefxgph.supabase.co';
-//const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaXVsd2NxeXhndnBpZWZ4Z3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MDMwMzgsImV4cCI6MjA4NjM3OTAzOH0.GlE4Q71Ja_vfDKhBF4j9pEFIIVnkaM8rPrmdRnibmmg';
-const _0x5f2a = 'aHR0cHM6Ly95Zml1bHdjcXl4Z3ZwaWVmeGdwaC5zdXBhYmFzZS5jbw==';
-const _0x9b7c = 'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW5bG1hZFd4M1kzRjVlR2QyY0dsbFpuaG5jR2dpTENKeWIyeGxJam9pWVc1dmJpSXNDbWxoZGNJNk1UYzNNRGd3TXpBek9DSXNaWGh3SWpveU1EZzJNemM1TURNNEZ3MC5HbEU0UTcxSmFfdmZES2hCRjRqOXBFRklJVm5rYU04clBybWRSbmlibW1n';
+const SUPABASE_URL = 'https://yfiulwcqyxgvpiefxgph.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaXVsd2NxeXhndnBpZWZ4Z3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MDMwMzgsImV4cCI6MjA4NjM3OTAzOH0.GlE4Q71Ja_vfDKhBF4j9pEFIIVnkaM8rPrmdRnibmmg';
 
-// Decode function (Simplified for ASCII)
-function _0x1d4e(str) {
-    return atob(str);
-}
 
-const SUPABASE_URL = _0x1d4e(_0x5f2a);
-const SUPABASE_KEY = _0x1d4e(_0x9b7c);
 
 
