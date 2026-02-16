7d5ed713645dd303ed5340a19dfa200aaba99ae8
cucuv007 <42567303+cucuv007@users.noreply.github.com>
2026-02-16 09:34:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,16 @@
 
-const SUPABASE_URL = 'https://yfiulwcqyxgvpiefxgph.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaXVsd2NxeXhndnBpZWZ4Z3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MDMwMzgsImV4cCI6MjA4NjM3OTAzOH0.GlE4Q71Ja_vfDKhBF4j9pEFIIVnkaM8rPrmdRnibmmg';
+//const SUPABASE_URL = 'https://yfiulwcqyxgvpiefxgph.supabase.co';
+//const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaXVsd2NxeXhndnBpZWZ4Z3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MDMwMzgsImV4cCI6MjA4NjM3OTAzOH0.GlE4Q71Ja_vfDKhBF4j9pEFIIVnkaM8rPrmdRnibmmg';
+const _0x5f2a = 'aHR0cHM6Ly95Zml1bHdjcXl4Z3ZwaWVmeGdwaC5zdXBhYmFzZS5jbw==';
+const _0x9b7c = 'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW5bG1hZFd4M1kzRjVlR2QyY0dsbFpuaG5jR2dpTENKeWIyeGxJam9pWVc1dmJpSXNDbWxoZGNJNk1UYzNNRGd3TXpBek9DSXNaWGh3SWpveU1EZzJNemM1TURNNEZ3MC5HbEU0UTcxSmFfdmZES2hCRjRqOXBFRklJVm5rYU04clBybWRSbmlibW1n';
+
+// Decode function
+function _0x1d4e(str) {
+    return decodeURIComponent(atob(str).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
+const SUPABASE_URL = _0x1d4e(_0x5f2a);
+const SUPABASE_KEY = _0x1d4e(_0x9b7c);
 
