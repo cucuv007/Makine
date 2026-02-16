afbeb332c797a1d7c26638bebec2b0fcb16b3624
cucuv007 <42567303+cucuv007@users.noreply.github.com>
2026-02-16 09:03:24 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase Configuration
+// NOT: Bu anahtarlar client-side (tarayici) tarafli calistigi icin kaynak kodda gorunur olmak zorundadir.
+// Supabase guvenligi Row Level Security (RLS) politikalari ile saglanir.
+const SUPABASE_URL = 'https://yfiulwcqyxgvpiefxgph.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaXVsd2NxeXhndnBpZWZ4Z3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MDMwMzgsImV4cCI6MjA4NjM3OTAzOH0.GlE4Q71Ja_vfDKhBF4j9pEFIIVnkaM8rPrmdRnibmmg';
